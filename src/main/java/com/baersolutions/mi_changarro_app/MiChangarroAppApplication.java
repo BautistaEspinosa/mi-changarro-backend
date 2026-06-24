a9c1c6b48149e4c6d36edc5bb5cfcf333545b2eb
BautistaEspinosa <bautistaespinosaroman@gmail.com>
2026-06-24 10:06:30 -0600

    chore: initial spring boot setup

---
+++ src/main/java/com/baersolutions/mi_changarro_app/MiChangarroAppApplication.java
@@ -0,0 +1,13 @@
+package com.baersolutions.mi_changarro_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiChangarroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiChangarroAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baersolutions/mi_changarro_app/MiChangarroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baersolutions.mi_changarro_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiChangarroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
